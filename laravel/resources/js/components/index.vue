245de1536d3b21c57d5a8ae35264909106f1a38f
伊藤翼 <tsubasa8770@gmail.com>
2021-02-10 17:10:48 +0900

    Vue Routerの導入・一覧ページ・記事作成ページ機能の実装

---
+++ laravel/resources/js/components/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <router-link to='/create'>記事作成</router-link>
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        this is index.vue
+                        <div v-for="message in messages" :key="message.id">
+                            <div class="blog-card" >
+                                {{ message.content }}
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                messages: [
+                    {
+                        content: ''
+                    }
+                ]
+            }
+        },
+        created() {
+            const uri = '/api';
+            axios.get(uri).then(response => {
+               this.messages = response.data;
+        });
+       },
+
+    }
+</script>
