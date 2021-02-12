3c3275eecf75fc2b9a840d26cab614af046a9116
伊藤翼 <tsubasa8770@gmail.com>
2021-02-12 16:14:57 +0900

    Vueを使いつつ、Laravelのユーザー登録とログイン機能

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('/login', 'Auth\LoginController@login');
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('/register', 'Auth\RegisterController@register');
+
+
 Route::get('/{any}', function () {
     return view('layouts.app');
 })->where('any', '.*');
