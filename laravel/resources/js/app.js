42d2717ff12da40f5cc4de61af64eb7b62dfe86a
伊藤翼 <tsubasa8770@gmail.com>
2021-02-19 17:00:07 +0900

    同様の内容の追加箇所の修正

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -3,12 +3,8 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 //components
-import Index from './components/index.vue';
-
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('index-component', require('./components/index.vue').default);
 
 new Vue({
     el: '#app',
-    router: router
 })
